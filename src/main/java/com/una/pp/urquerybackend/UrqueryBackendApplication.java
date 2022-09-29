058c5b04de4b591a820551b88dc4341dce28c37d
albnacho <albcarlosrndn@gmail.com>
2022-09-29 16:25:51 -0600

    Spring initialized

---
+++ src/main/java/com/una/pp/urquerybackend/UrqueryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.una.pp.urquerybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrqueryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrqueryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/una/pp/urquerybackend/UrqueryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.una.pp.urquerybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrqueryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
